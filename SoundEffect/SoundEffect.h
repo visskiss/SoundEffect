4fa3484470d6dd866c61d6a320c2d4cdc9bc97c4
Daniel Kanaan <dkanaan@conversantmedia.com>
2016-03-07 09:06:33 -0800

    Initial Commit

---
+++ SoundEffect/SoundEffect.h
@@ -0,0 +1,19 @@
+//
+//  SoundEffect.h
+//  SoundEffect
+//
+//  Created by Daniel Kanaan on 3/7/16.
+//  Copyright © 2016 Daniel Kanaan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SoundEffect.
+FOUNDATION_EXPORT double SoundEffectVersionNumber;
+
+//! Project version string for SoundEffect.
+FOUNDATION_EXPORT const unsigned char SoundEffectVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SoundEffect/PublicHeader.h>
+
+
